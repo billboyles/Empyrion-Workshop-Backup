f3b26658d142df34286a2895e748b48402251dda
billboyles <38093848+billboyles@users.noreply.github.com>
2022-05-20 19:04:01 -0500

    Refactoring and UI updates
    Refactoring and updates to output and UI

--- ewb.py
+++ ewb.py
@@ -1,57 +1,89 @@
 import requests, re, os, shutil, tkinter
 from bs4 import BeautifulSoup
 from tkinter import filedialog, simpledialog, messagebox
-
-root = tkinter.Tk()
-root.title("Empyrion Workshop Backup")
-root.withdraw()
-
-backup_path = tkinter.filedialog.askdirectory(title="Please select the desired backup folder", initialdir="C:/")
-tkinter.messagebox.showinfo(title="Selected file", message="Selected: " + backup_path)
-
 epb_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/383120"
-epb_dirs = os.listdir(epb_path)
 
-steam_id = tkinter.simpledialog.askstring(title="Steam ID", prompt="Please enter the Steam ID to be backed up")
-workshop_url = "https://steamcommunity.com/profiles/" + steam_id + "/myworkshopfiles/?appid=383120"
+def get_backup_path():
+	backup_path = tkinter.filedialog.askdirectory(title="Please select the desired backup folder", initialdir="C:/")
+	if backup_path:
+		tkinter.messagebox.showinfo(title="Selected file", message="Selected: " + backup_path)
+	else: 
+		tkinter.messagebox.showerror(title="Error", message="No backup path selected")
+	return backup_path
 
-urls = []
+def get_epb_dirs(epb_path):
+	epb_dirs = os.listdir(epb_path)
+	return epb_dirs
 
-r = requests.get(workshop_url + "&p=1&numperpage=30").text
-soup = BeautifulSoup(r, "html.parser")
-q, mod = divmod(int(soup.find("div", class_="workshopBrowsePagingInfo").string.split()[3]), 30)
-pages = q if mod == 0 else  q + 1
-for n in range(0, pages):
-	print(workshop_url + "&p=" + str(n + 1) + "&numperpage=30")
-	r = requests.get(workshop_url + "&p=" + str(n + 1) + "&numperpage=30").text
+def get_steam_id():
+	#TODO: hasndle invalid Steam IDs
+	steam_id = tkinter.simpledialog.askstring(title="Steam ID", prompt="Please enter the Steam ID to be backed up")
+	return steam_id
+
+def get_workshop_url(steam_id):
+	workshop_url = "https://steamcommunity.com/profiles/" + steam_id + "/myworkshopfiles/?appid=383120"
+	return workshop_url
+
+def get_build_urls(workshop_url):
+	urls = []
+	r = requests.get(workshop_url + "&p=1&numperpage=30").text
 	soup = BeautifulSoup(r, "html.parser")
-	links = soup.select("div.workshopItem > a.ugc")
-	for link in links:
-		if link.has_attr("href"):
-			urls.append(link['href'])
+	q, mod = divmod(int(soup.find("div", class_="workshopBrowsePagingInfo").string.split()[3]), 30)
+	pages = q if mod == 0 else q + 1
+	for n in range(0, pages):
+		print(workshop_url + "&p=" + str(n + 1) + "&numperpage=30")
+		r = requests.get(workshop_url + "&p=" + str(n + 1) + "&numperpage=30").text
+		soup = BeautifulSoup(r, "html.parser")
+		links = soup.select("div.workshopItem > a.ugc")
+		for link in links:
+			if link.has_attr("href"):
+				urls.append(link['href'])
+	return urls
 
-i = 1
-for url in urls:
-	print("trying ", i)
-	r = requests.get(url).text
-	soup = BeautifulSoup(r, "html.parser")
-	title = re.findall("::(.*)<", str(soup.find("title")))[0].replace(":", " -").replace("/", "-")
-	print(title)
-	path = os.path.join(backup_path, title)
-	os.mkdir(path)
-	description = soup.find("div", class_="workshopItemDescription").get_text()
-	with open(path + "/" + title + ".txt", "w") as f:
-		f.write(description)
-	imgs = soup.select("div.highlight_strip_screenshot > img")
-	n = 1
-	for img in imgs:
-		with open(path + "/" + title + str(n) + ".jpg", "wb") as f:
-			f.write(requests.get(img['src'].split("?")[0]).content)
-		n += 1
-	print("download completed, moving epb")
-	for epb_dir in epb_dirs:
-		files = os.listdir(os.path.join(epb_path, epb_dir))
-		for file in files:
-			if file.split(".")[0] == title:
-				shutil.copy2(os.path.join(epb_path, epb_dir, file), path)
-	i += 1
+def backup(backup_path, epb_path, epb_dirs, urls):
+	#TODO: move this into tkinter
+	i = 1
+	for url in urls:
+		print(f"trying {i}...")
+		r = requests.get(url).text
+		soup = BeautifulSoup(r, "html.parser")
+		title = re.findall("::(.*)<", str(soup.find("title")))[0].replace(":", " -").replace("/", "-")
+		print(f"Found build: {title}")
+		print("Starting download from Steam...")
+		path = os.path.join(backup_path, title)
+		os.mkdir(path)
+		description = soup.find("div", class_="workshopItemDescription").get_text()
+		with open(path + "/" + title + ".txt", "w") as f:
+			f.write(description)
+		imgs = soup.select("div.highlight_strip_screenshot > img")
+		n = 1
+		for img in imgs:
+			with open(path + "/" + title + str(n) + ".jpg", "wb") as f:
+				f.write(requests.get(img['src'].split("?")[0]).content)
+			n += 1
+		print("Download completed, moving epb...")
+		for epb_dir in epb_dirs:
+			files = os.listdir(os.path.join(epb_path, epb_dir))
+			for file in files:
+				if file.split(".")[0] == title:
+					shutil.copy2(os.path.join(epb_path, epb_dir, file), path)
+		print(f"Moving {title} completed")
+		i += 1
+
+def main():
+	#TODO: make this all work in one tkinter window
+	root = tkinter.Tk()
+	root.title("Empyrion Workshop Backup")
+	root.withdraw()
+
+	backup_path = get_backup_path()
+	if not backup_path:
+		return
+	epb_dirs = get_epb_dirs(epb_path)
+	steam_id = get_steam_id()
+	workshop_url = get_workshop_url(steam_id)
+	urls = get_build_urls(workshop_url)
+	backup(backup_path, epb_path, epb_dirs, urls)
+
+if __name__ == "__main__":
+    main()
